ab762d84fb89c6482784469ced6de3e109f98ac5
dani-ideas <daniideas0@gmail.com>
2025-01-10 16:54:18 -0600

    hot fix

--- setting_up_user_accounts/ll_project/settings.py
+++ setting_up_user_accounts/ll_project/settings.py
@@ -129,4 +129,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # My settings.
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
-LOGIN_URL = 'accounts:login'
